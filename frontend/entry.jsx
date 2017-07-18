66e5357f58206e89b06d8afe1dd694a98238dd49
Alex Milbert <alex.milbert@gmail.com>
2017-07-18 09:28:26 -0700

    backend auth

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  ReactDOM.render(<Root store={store} />, root);
+  ReactDOM.render(<h1>Tuesday</h1>, root);
 });
