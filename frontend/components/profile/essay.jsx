204c35f0a3b4d4a35d8a50644855fed83b416993
Alex Milbert <alex.milbert@gmail.com>
2017-07-20 09:29:35 -0700

    essay components update

--- frontend/components/profile/essay.jsx
+++ frontend/components/profile/essay.jsx
@@ -3,6 +3,35 @@ import EssayDetail from './essay_detail';
 
 class Essay extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
+  }
+
+  render() {
+    it (this.props.profile) {
+      return(
+        <div className="profile-content">
+          <EssayDetail
+            detailName="summary"
+            detailTitle="Summary Title"
+            currentUser={ this.props.currentUser }
+            userInput={ this.props.profile.summary }
+            profile={ this.props.profile }
+            placeHolder="Please provide your food bio"
+            updateProfile={ this.props.updateProfile } />
+          <EssayDetail
+            detailName="friday-grub"
+            detailTitle="On a typical Friday night I am eating"
+            currentUser={ this.props.currentUser }
+            userInput={ this.props.profile.friday }
+            profile={ this.props.profile }
+            placeHolder="Late night tacos or five star cuisine"
+            updateProfile={ this.props.updateProfile } />
+        </div>
+      );
+    } else {
+      return <div></div>
+    }
   }
 }
+
+export default Essay;
---
+++ frontend/components/profile/essay_detail.jsx
@@ -0,0 +1,157 @@
+import React from 'react';
+
+class EssayDetail extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      previousInput: "",
+      userInput: "",
+      edit: false
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleUpdate = this.handleUpdate.bind(this);
+    this.handleCancel = this.handleCancel.bind(this);
+
+    this.ownDetail = this.ownDetail.bind(this);
+    this.normalDetail = this.normalDetail.bind(this);
+
+    this.editText = this.editText.bind(this);
+    this.normalText = this.normalText.bind(this);
+    this.currentText = this.currentText.bind(this);
+
+  }
+
+  componentDidMount() {
+    this.setState({ userInput: nextProps.userInput });
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({ userInput: nextProps.userInput });
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+
+    this.setState({ previousInput: this.state.userInput, edit: true});
+  }
+
+  handleUpdate(e) {
+    e.preventDefault();
+
+    const updatedDetail = { [this.props.detailName]: this.state.userInput };
+
+    const user = Object.assign({}, this.props.profile, updatedDetail);
+
+    this.props.updateProfile(user).then(() => {
+      this.setState({ edit: false });
+    });
+  }
+
+  handleChange(e) {
+    this.setState({ userInput: e.currentTarget.value });
+  }
+
+  handleCancel(e) {
+    e.preventDefault();
+
+    this.setState({ userInput: this.state.previousInput, previousInput: "", edit: false });
+  }
+
+  normalDetail() {
+    return(
+      <div>
+        <div className="essay-normal-group">
+
+          <span className="essay-title">{this.props.detailTitle}</span>
+
+        </div>
+      </div>
+    );
+  }
+
+  ownDetail() {
+    return(
+      <div>
+
+        <button className="essay-button" onClick={ this.handleClick }>
+          <span className="essay-title">{this.props.detailTitle}</span>
+          <span className="essay-edit-pencil"></span>
+        </button>
+
+      </div>
+    );
+  }
+
+  editText() {
+    return (
+      <form className="edit-text">
+        <textarea
+          className="edit-text-inside"
+          type="text"
+          placeholder={ this.props.placeHolder }
+          onChange={ this.handleChange }
+          value={ this.state.userInput }
+
+          <input type="submit" value="Save" className="save-button" onClick={ this.handleUpdate }/ >
+          <input type="submit" value="Cancel" className="cancel-button" onClick={ this.handleCancel } />
+
+      </form>
+    );
+  }
+
+  normalText() {
+    let placeHolderText = this.state.userInput;
+    let placeHolderBool = false;
+
+    if (!this.state.userInput) {
+      placeHolderText = this.props.placeHolder;
+      placeHolderBool = true;
+    } else if (this.props.currentUser.id === this.props.profile.id && this.state.userInput.length === 0) {
+      placeHolderText = this.props.placeHolder;
+      placeHolderBool = true;
+    }
+
+    if (placeHolderBool) {
+      return (
+        <div className="placeholder-text">{ placeHolderText }</div>
+      );
+    } else {
+      return (
+        <div className="essay-text">{ this.state.userInput }</div>
+      );
+    }
+  }
+
+  currentText() {
+    if (!this.state.edit) {
+      return this.editText();
+    } else {
+      return this.normalText();
+    }
+  }
+
+  render() {
+    if (!this.props.currentUser) {
+      return null;
+    } else if (this.props.currentUser.id == this.props.profile.id) {
+      return (
+        <div className="essay-block">
+          {this.ownDetail()}
+          {this.currentText()}
+        </div>
+      );
+    } else {
+      return (
+        <div className="essay-block">
+          {this.normalDetail()}
+          {this.normalText()}
+        </div>
+      );
+    }
+  }
+  }
+
+  export default EssayDetail;
