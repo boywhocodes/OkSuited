e0e330300ae358b80e3cfb172430b60c3632eb7d
Alex Milbert <alex.milbert@gmail.com>
2017-07-23 12:46:24 -0700

    migrations and db for q & r

--- db/migrate/20170723182524_create_responses.rb
+++ db/migrate/20170723182524_create_responses.rb
@@ -1,7 +1,11 @@
 class CreateResponses < ActiveRecord::Migration[5.0]
   def change
     create_table :responses do |t|
-
+      t.integer :choice_id, null: false
+      t.integer :user_id, null: false
+      t.string :acceptable_choices, default: [], null: false, array: true
+      t.integer :importance
+      t.text :explanation
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170720184448) do
+ActiveRecord::Schema.define(version: 20170723182524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "choices", force: :cascade do |t|
+    t.integer  "question_id", null: false
+    t.text     "body",        null: false
+    t.integer  "order",       null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "responses", force: :cascade do |t|
+    t.integer  "choice_id",                       null: false
+    t.integer  "user_id",                         null: false
+    t.string   "acceptable_choices", default: [], null: false, array: true
+    t.integer  "importance"
+    t.text     "explanation"
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username",           null: false
