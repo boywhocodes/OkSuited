1491f8fe586b6cdb55cc03692a08822ba479b14f
Alex Milbert <alex.milbert@gmail.com>
2017-08-29 15:47:20 -0700

    search configuration tweaks

--- frontend/components/user/user_index.jsx
+++ frontend/components/user/user_index.jsx
@@ -15,15 +15,15 @@ class UserIndex extends React.Component {
 
 
     this.display = this.display.bind(this);
-    this.sortedUsers = this.sortedUsers.bind(this);
+    // this.sortedUsers = this.sortedUsers.bind(this);
     this.userIndexItems = this.userIndexItems.bind(this);
 
     // this.preferences = this.preferences.bind(this);
     // this.distanceOptions = this.distanceOptions.bind(this);
-    this.handleDistance = this.handleDistance.bind(this);
-    this.findMatchPercentage = this.findMatchPercentage.bind(this);
-    this.calculateQuestionImportance = this.calculateQuestionImportance.bind(this);
-    this.calculateQuestionScore = this.calculateQuestionScore.bind(this);
+    // this.handleDistance = this.handleDistance.bind(this);
+    // this.findMatchPercentage = this.findMatchPercentage.bind(this);
+    // this.calculateQuestionImportance = this.calculateQuestionImportance.bind(this);
+    // this.calculateQuestionScore = this.calculateQuestionScore.bind(this);
     this.matchSearch = this.matchSearch.bind(this);
     this.updateAge = this.updateAge.bind(this);
     // this.updateEatingSpeed = this.updateEatingSpeed.bind(this);
@@ -34,7 +34,7 @@ class UserIndex extends React.Component {
   componentDidMount() {
     this.props.fetchUsers(this.state.distance);
     this.setState({ distance: 3000 });
-    this.props.fetchQuestions();
+  //   this.props.fetchQuestions();
   }
 
 
@@ -70,23 +70,6 @@ matchSearch() { return (
     )
   }
 
-
-
-
-  sortedUsers() {
-    return Object.keys(this.props.users).map(user => {
-      const matchPercentage = this.findMatchPercentage(this.props.users[user]);
-      return { user, matchPercentage };
-    }).filter(user =>
-      this.Preference(user)
-    );
-  }
-
-  handleDistance(e) {
-    this.props.fetchUsers(parseInt(e.currentTarget.value));
-    this.setState({ distance: parseInt(e.currentTarget.value) });
-  }
-
   userIndexItems() {
     const user = this.props.users
     const matches = Object.keys(this.props.users).map(user_id => {
@@ -100,106 +83,6 @@ matchSearch() { return (
       );
     });
     return matches;
-  }
-
-  findMatchPercentage(user) {
-    let currentUserPoints = 0;
-    let currentUserQuestionTotal = 0;
-    let otherUserPoints = 0;
-    let otherUserQuestionTotal = 0;
-
-    const userQuestions = this.props.currentUser.questions.map((question) => { //questions aren't nested in currentUser.
-      return question.id;
-    });
-
-    const otherUserQuestions = user.questions.map((question) => {
-      return question.id;
-    });
-
-    const commonQuestions = Object.keys(this.props.questions).map((question) => {
-      if (userQuestions.includes(parseInt(question)) && otherUserQuestions.includes(parseInt(question))) {
-        return this.props.questions[question];
-      }
-    }).filter((question) => question !== undefined);
-
-    if (commonQuestions.length === 0) {
-      return 0;
-    }
-
-    commonQuestions.forEach((question) => {
-      currentUserPoints += this.calculateQuestionScore(question, this.props.currentUser, user);
-      currentUserQuestionTotal += this.calculateQuestionImportance(question, this.props.currentUser);
-      otherUserPoints += this.calculateQuestionScore(question, user, this.props.currentUser);
-      otherUserQuestionTotal += this.calculateQuestionImportance(question, user);
-    });
-
-    const currentUserPercent = (currentUserPoints / currentUserQuestionTotal);
-    const otherUserPercent = (otherUserPoints / otherUserQuestionTotal);
-
-    const multiplied = currentUserPercent * otherUserPercent;
-    const root = commonQuestions.length;
-    let matchPercent = Math.floor((Math.sqrt(multiplied) - (1 / (2 * root))) * 100);
-
-    if (matchPercent < 0) {
-      matchPercent = 0;
-    }
-    return matchPercent;
-  }
-
-  calculateQuestionImportance(question, user) {
-    let questionImportance = 0;
-    const choices = question.choices.map((choice) => choice.id);
-
-    user.responses.forEach((response) => {
-      if (choices.includes(response.choice_id)) {
-        questionImportance = response.importance; //can I pass down importance like this?
-      }
-    });
-
-    return questionImportance;
-  }
-
-  calculateQuestionScore(question, user, otherUser) {
-    const choices = question.choices;
-    const choiceIds = question.choices.map((choice) => choice.id);
-    let otherUserChoices = null;
-    let userAcceptables = null;
-    let userImportance = 0;
-
-    choices.forEach((choice) => {
-      otherUser.responses.forEach((response) => {
-        if (response.choice_id === choice.id) {
-          otherUserChoices = choice.body;
-        }
-      });
-    });
-
-    user.responses.forEach((response) => {
-      if (choiceIds.includes(response.choice_id)) {
-        userAcceptables = response.acceptable_choices;
-        userImportance = response.importance;
-      }
-    });
-
-    if (userAcceptables.includes(otherUserChoices)) {
-      return userImportance;
-    } else {
-      return 0;
-    }
-
-  }
-
-  distanceOptions() {
-    return (
-      <select className="sort-dropdown" onChange={ this.handleDistance }>
-        <option value="3000">500+</option>
-        <option value="2">2</option>
-        <option value="5">5</option>
-        <option value="10">10</option>
-        <option value="50">50</option>
-        <option value="100">100</option>
-      </select>
-    );
   }
 
   display() {
@@ -227,6 +110,128 @@ matchSearch() { return (
 }
 
 export default withRouter(UserIndex);
+  //
+  //
+  // sortedUsers() {
+  //   return Object.keys(this.props.users).map(user => {
+  //     const matchPercentage = this.findMatchPercentage(this.props.users[user]);
+  //     return { user, matchPercentage };
+  //   }).filter(user =>
+  //     this.Preference(user)
+  //   );
+  // }
+  //
+  // handleDistance(e) {
+  //   this.props.fetchUsers(parseInt(e.currentTarget.value));
+  //   this.setState({ distance: parseInt(e.currentTarget.value) });
+  // }
+  //
+  //
+  //
+  // findMatchPercentage(user) {
+  //   let currentUserPoints = 0;
+  //   let currentUserQuestionTotal = 0;
+  //   let otherUserPoints = 0;
+  //   let otherUserQuestionTotal = 0;
+  //
+  //   const userQuestions = this.props.currentUser.questions.map((question) => { //questions aren't nested in currentUser.
+  //     return question.id;
+  //   });
+  //
+  //   const otherUserQuestions = user.questions.map((question) => {
+  //     return question.id;
+  //   });
+  //
+  //   const commonQuestions = Object.keys(this.props.questions).map((question) => {
+  //     if (userQuestions.includes(parseInt(question)) && otherUserQuestions.includes(parseInt(question))) {
+  //       return this.props.questions[question];
+  //     }
+  //   }).filter((question) => question !== undefined);
+  //
+  //   if (commonQuestions.length === 0) {
+  //     return 0;
+  //   }
+  //
+  //   commonQuestions.forEach((question) => {
+  //     currentUserPoints += this.calculateQuestionScore(question, this.props.currentUser, user);
+  //     currentUserQuestionTotal += this.calculateQuestionImportance(question, this.props.currentUser);
+  //     otherUserPoints += this.calculateQuestionScore(question, user, this.props.currentUser);
+  //     otherUserQuestionTotal += this.calculateQuestionImportance(question, user);
+  //   });
+  //
+  //   const currentUserPercent = (currentUserPoints / currentUserQuestionTotal);
+  //   const otherUserPercent = (otherUserPoints / otherUserQuestionTotal);
+  //
+  //   const multiplied = currentUserPercent * otherUserPercent;
+  //   const root = commonQuestions.length;
+  //   let matchPercent = Math.floor((Math.sqrt(multiplied) - (1 / (2 * root))) * 100);
+  //
+  //   if (matchPercent < 0) {
+  //     matchPercent = 0;
+  //   }
+  //   return matchPercent;
+  // }
+  //
+  // calculateQuestionImportance(question, user) {
+  //   let questionImportance = 0;
+  //   const choices = question.choices.map((choice) => choice.id);
+  //
+  //   user.responses.forEach((response) => {
+  //     if (choices.includes(response.choice_id)) {
+  //       questionImportance = response.importance; //can I pass down importance like this?
+  //     }
+  //   });
+  //
+  //   return questionImportance;
+  // }
+  //
+  // calculateQuestionScore(question, user, otherUser) {
+  //   const choices = question.choices;
+  //   const choiceIds = question.choices.map((choice) => choice.id);
+  //   let otherUserChoices = null;
+  //   let userAcceptables = null;
+  //   let userImportance = 0;
+  //
+  //   choices.forEach((choice) => {
+  //     otherUser.responses.forEach((response) => {
+  //       if (response.choice_id === choice.id) {
+  //         otherUserChoices = choice.body;
+  //       }
+  //     });
+  //   });
+  //
+  //   user.responses.forEach((response) => {
+  //     if (choiceIds.includes(response.choice_id)) {
+  //       userAcceptables = response.acceptable_choices;
+  //       userImportance = response.importance;
+  //     }
+  //   });
+  //
+  //   if (userAcceptables.includes(otherUserChoices)) {
+  //     return userImportance;
+  //   } else {
+  //     return 0;
+  //   }
+  //
+  // }
+  //
+  // distanceOptions() {
+  //   return (
+  //     <select className="sort-dropdown" onChange={ this.handleDistance }>
+  //       <option value="3000">500+</option>
+  //       <option value="2">2</option>
+  //       <option value="5">5</option>
+  //       <option value="10">10</option>
+  //       <option value="50">50</option>
+  //       <option value="100">100</option>
+  //     </select>
+  //   );
+  // }
+
+
+
+
+
 
 // <div className="browse-match-search"><input type="submit" value="Search for Matches"/></div>
 
